16eb64210445e14ffc8832a8cda14a48e80f2304
Raphael Lullis <raphael@mushroomlabs.com>
2022-05-24 11:34:24 +0200

    clean permissions module

--- hub20/apps/core/permissions.py
+++ hub20/apps/core/permissions.py
@@ -1,13 +1,14 @@
 from rest_framework import permissions
 
 
-class IsStoreOwnerOrAnonymousReadOnly(permissions.IsAuthenticatedOrReadOnly):
-    def has_object_permission(self, request, view, obj):
-        is_safe = request.method in permissions.SAFE_METHODS
-
-        return obj.owner == request.user if request.user.is_authenticated else is_safe
-
-
 class IsStoreOwner(permissions.IsAuthenticated):
     def has_object_permission(self, request, view, obj):
         return request.user.is_authenticated and obj.owner == request.user
+
+
+class IsAdminOrReadOnly(permissions.IsAuthenticated):
+    def has_permission(self, request, view):
+        if request.method.upper() in permissions.SAFE_METHODS:
+            return True
+
+        return request.user.is_staff
