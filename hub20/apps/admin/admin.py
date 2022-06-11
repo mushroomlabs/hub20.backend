35b568241003d73b6d2986ac6cfc0a0a5c767bf4
Raphael Lullis <raphael@mushroomlabs.com>
2022-06-11 02:54:37 +0200

    (chore) re-add User model to admin main page

--- hub20/apps/admin/admin.py
+++ hub20/apps/admin/admin.py
@@ -1,8 +1,14 @@
 from django.apps import apps
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # De-register all models from other apps
 for app_config in apps.get_app_configs():
     for model in app_config.get_models():
         if admin.site.is_registered(model):
             admin.site.unregister(model)
+
+
+admin.site.register(User)
