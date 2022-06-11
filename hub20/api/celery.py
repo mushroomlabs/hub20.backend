7d8b3f0b8d79b4937cfb59db808b87655431ad47
Raphael Lullis <raphael@mushroomlabs.com>
2022-06-11 02:50:37 +0200

    (chore) re-introduce execute_pending_transfers to scheduled tasks

--- hub20/api/celery.py
+++ hub20/api/celery.py
@@ -10,37 +10,17 @@ class Hub20CeleryConfig:
 
     broker_url = "memory" if "HUB20_TEST" in os.environ else settings.CELERY_BROKER_URL
     broker_use_ssl = "HUB20_BROKER_USE_SSL" in os.environ
+    beat_scheduler = "django_celery_beat.schedulers:DatabaseScheduler"
     beat_schedule = {
         "clear-expired-sessions": {
             "task": "hub20.apps.core.tasks.clear_expired_sessions",
             "schedule": crontab(minute="*/30"),
             "expires": 60,
         },
-        # "refresh-wallet-balances": {
-        #     "task": "hub20.apps.wallet.tasks.update_all_wallet_balances",
-        #     "schedule": crontab(minute=0),
-        #     "expires": BLOCK_CREATION_INTERVAL,
-        # },
-        # "execute-transfers": {
-        #     "task": "hub20.apps.core.tasks.execute_pending_transfers",
-        #     "schedule": crontab(),
-        #     "expires": 60,
-        # },
-        # "sync-raiden-channels": {
-        #     "task": "hub20.apps.raiden.tasks.sync_channels",
-        #     "schedule": crontab(minute="*/5"),
-        #     "expires": 5 * 60,
-        # },
-        # "sync-raiden-payments": {
-        #     "task": "hub20.apps.raiden.tasks.sync_payments",
-        #     "schedule": timedelta(seconds=RAIDEN_PAYMENT_CHECK_INTERVAL),
-        #     "expires": RAIDEN_PAYMENT_CHECK_INTERVAL,
-        # },
-        # "check-payments-in-open-routes": {
-        #     "task": "hub20.apps.core.tasks.check_payments_in_open_routes",
-        #     "schedule": timedelta(seconds=BLOCK_CREATION_INTERVAL),
-        #     "expires": BLOCK_CREATION_INTERVAL,
-        # },
+        "execute-transfers": {
+            "task": "hub20.apps.core.tasks.execute_pending_transfers",
+            "schedule": 15,
+        },
     }
     result_backend = "django-db"
     task_always_eager = "HUB20_TEST" in os.environ
--- hub20/apps/raiden/api.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import SimpleRouter
-
-from . import views
-
-router = SimpleRouter(trailing_slash=False)
-router.register("nodes", views.RaidenViewSet, basename="raiden")
-
-urlpatterns = router.urls
