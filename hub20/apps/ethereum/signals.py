c4a7b4bdd28c4620298715e22f3e9183cbcc4ea9
Raphael Lullis <raphael@mushroomlabs.com>
2022-06-27 02:01:54 +0200

    (chore) remove ununsed signal definitions

--- hub20/apps/ethereum/signals.py
+++ hub20/apps/ethereum/signals.py
@@ -6,13 +6,11 @@ chain_reorganization_detected = Signal(["chain_id", "new_block_height"])
 incoming_transfer_broadcast = Signal(["account", "amount", "transaction_data"])
 outgoing_transfer_broadcast = Signal(["account", "amount", "transaction_data"])
 outgoing_transfer_mined = Signal(["account", "transaction", "amount", "address"])
-blockchain_route_requested = Signal(["deposit"])
 wallet_generated = Signal(["wallet"])
 
 
 __all__ = [
     "block_sealed",
-    "blockchain_route_requested",
     "chain_status_synced",
     "chain_reorganization_detected",
     "incoming_transfer_broadcast",
--- hub20/apps/raiden/signals.py
+++ hub20/apps/raiden/signals.py
@@ -2,4 +2,3 @@ from django.dispatch import Signal
 
 raiden_payment_received = Signal(["payment"])
 raiden_payment_sent = Signal(["payment"])
-service_deposit_sent = Signal(["transaction", "raiden", "amount", "contract_address"])
